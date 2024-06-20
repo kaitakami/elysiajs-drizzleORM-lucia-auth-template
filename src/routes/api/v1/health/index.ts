3e6d47c5da421f6e4b6f4b016fee74e77d6713f8
kaitakami <kaitakami@protonmail.com>
2024-06-21 01:31:34 +0900

    feat: create health endpoints

---
+++ src/routes/api/v1/health/index.ts
@@ -0,0 +1,50 @@
+import { t } from "elysia";
+import os from "node:os";
+
+import type { App } from "@/index";
+
+export default (app: App) => {
+	app.get("/", () => ({ health: "ok" }), {
+		response: t.Object({
+			health: t.String({
+				description: "Returns ok for health check",
+			}),
+		}),
+		detail: {
+			description: "The root endpoint",
+			tags: ["Health"],
+		},
+	});
+
+	app.get(
+		"/info",
+		() => {
+			const cpuUsage = process.cpuUsage();
+			const totalMemory = os.totalmem();
+			const freeMemory = os.freemem();
+
+			return {
+				cpuUsage,
+				totalMemory,
+				freeMemory,
+			};
+		},
+		{
+			response: t.Object({
+				cpuUsage: t.Object({
+					user: t.Number(),
+					system: t.Number(),
+				}),
+				totalMemory: t.Number(),
+				freeMemory: t.Number(),
+			}),
+			detail: {
+				description: "The info endpoint",
+				tags: ["Health"],
+			},
+		},
+	);
+
+	return app;
+};
+
