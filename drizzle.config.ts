d99f577f719d08d7edd9c28ae74d6fb3936be224
kaitakami <kaitakami@protonmail.com>
2024-06-21 08:44:15 +0900

    feat: create drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from '@/env';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+});
