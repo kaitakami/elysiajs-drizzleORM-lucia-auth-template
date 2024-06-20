dc248ea6c562aff5c8c0cf74482a6a4d32018c65
kaitakami <kaitakami@protonmail.com>
2024-06-21 01:30:04 +0900

    feat: add typesafety for env variables

--- src/env.ts
+++ src/env.ts
@@ -2,18 +2,17 @@ import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
 export const env = createEnv({
-  clientPrefix: "PUBLIC_",
-  server: {
-    DATABASE_URL: z.string().url(),
-  },
-  client: {
-  },
-  /**
-   * Makes sure you explicitly access **all** environment variables
-   * from `server` and `client` in your `runtimeEnv`.
-   */
-  runtimeEnvStrict: {
-    DATABASE_URL: process.env.DATABASE_URL,
-  },
-  emptyStringAsUndefined: true,
+	clientPrefix: "PUBLIC_",
+	server: {
+		DATABASE_URL: z.string().url(),
+	},
+	client: {},
+	/**
+	 * Makes sure you explicitly access **all** environment variables
+	 * from `server` and `client` in your `runtimeEnv`.
+	 */
+	runtimeEnvStrict: {
+		DATABASE_URL: process.env.DATABASE_URL,
+	},
+	emptyStringAsUndefined: true,
 });
