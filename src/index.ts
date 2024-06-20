0bffa30d8bd5f1b3b8ec5353a1f653ce0a8e955f
kaitakami <kaitakami@protonmail.com>
2024-06-21 01:30:18 +0900

    feat; add elysia with steroids

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
+import logger from "@/utils/logger";
+import cors from "@elysiajs/cors";
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
+import { autoroutes } from "elysia-autoroutes";
+import { db } from "./db";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(cors())
+	.use(swagger())
+	.use(
+		autoroutes({
+			routesDir: "./routes",
+		}),
+	)
+	.decorate("db", db)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	.listen(3000);
+
+logger.info(
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
+
+export { app };
+export type App = typeof app;
