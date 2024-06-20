2004956d446f51968a2262b7176ac1d599fb6584
kaitakami <kaitakami@protonmail.com>
2024-06-21 01:30:53 +0900

    feat: create auth tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,16 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const userTable = pgTable("user", {
+	id: text("id").primaryKey(),
+});
+
+export const sessionTable = pgTable("session", {
+	id: text("id").primaryKey(),
+	userId: text("user_id")
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: timestamp("expires_at", {
+		withTimezone: true,
+		mode: "date",
+	}).notNull(),
 });
