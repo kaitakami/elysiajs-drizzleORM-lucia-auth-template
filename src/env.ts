fa18be6e6d275f19e24d9fbbb3edb7f216aa0827
kaitakami <kaitakami@protonmail.com>
2024-06-21 11:07:41 +0900

    feat: add node env var

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,11 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+import 'dotenv/config'
 
 export const env = createEnv({
 	clientPrefix: "PUBLIC_",
 	server: {
+		NODE_ENV: z.enum(["development", "production", 'test']).default("development"),
 		DATABASE_URL: z.string().url(),
 		SERVER_PORT: z
 			.string()
@@ -16,6 +18,7 @@ export const env = createEnv({
 	 * from `server` and `client` in your `runtimeEnv`.
 	 */
 	runtimeEnvStrict: {
+		NODE_ENV: process.env.NODE_ENV,
 		DATABASE_URL: process.env.DATABASE_URL,
 		SERVER_PORT: process.env.SERVER_PORT,
 	},
