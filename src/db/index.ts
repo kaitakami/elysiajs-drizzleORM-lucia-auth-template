dd660e87ca016daae4d1f6cb7dfc5da47087b08c
kaitakami <kaitakami@protonmail.com>
2024-06-28 19:24:14 -0600

    fix: add tables types to db type

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
 import { env } from "@/env";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema"
 
 const connectionString = env.DATABASE_URL;
 
 const client = postgres(connectionString);
 
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
