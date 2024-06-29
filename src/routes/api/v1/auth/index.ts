af8b0f188e6849d40af228e67318419792052169
kaitakami <kaitakami@protonmail.com>
2024-06-28 21:22:35 -0600

    feat: add signout endpoint

--- src/routes/api/v1/auth/index.ts
+++ src/routes/api/v1/auth/index.ts
@@ -1,7 +1,11 @@
 import type { App } from "@/index";
 import signup from "./signup";
+import signin from "./signin";
+import signout from "./signout";
 
 export default (app: App) => {
 	app.use(signup);
+	app.use(signin);
+	app.use(signout);
 	return app;
 };
---
+++ src/routes/api/v1/auth/signout.ts
@@ -0,0 +1,41 @@
+import { t } from "elysia";
+import { lucia } from "@/auth/lucia";
+import { BadRequestException } from "@/plugins/error/exceptions";
+
+import type { App } from "@/index";
+
+export default (app: App) => {
+	app.post(
+		"/signout",
+		async ({ cookie, logger }) => {
+			logger.info(cookie, "Signout request");
+			const sessionCookie = cookie[lucia.sessionCookieName];
+
+			if (!sessionCookie?.value) {
+				logger.error("Session not found.");
+				throw new BadRequestException("Session not found.");
+			}
+
+			await lucia.invalidateSession(sessionCookie.value);
+			const blankSessionCookie = lucia.createBlankSessionCookie();
+
+			sessionCookie.set({
+				value: blankSessionCookie.value,
+				...blankSessionCookie.attributes,
+			});
+
+			logger.info("User signed out");
+			return { message: "Successfully signed out" };
+		},
+		{
+			response: t.Object({
+				message: t.String(),
+			}),
+			detail: {
+				description: "The signout endpoint, will invalidate the user's session and clear the session cookie.",
+			},
+		},
+	);
+
+	return app;
+};
