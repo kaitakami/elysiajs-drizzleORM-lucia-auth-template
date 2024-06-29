946cba5a995281258e2dbfd53020765ddf892b86
kaitakami <kaitakami@protonmail.com>
2024-06-28 19:23:48 -0600

    feat: fit lucia config with db schema

--- src/auth/lucia.ts
+++ src/auth/lucia.ts
@@ -2,9 +2,9 @@ import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { Lucia } from "lucia";
 
 import { db } from "@/db";
-import { sessionTable, userTable } from "@/db/schema";
+import { sessions, users } from "@/db/schema";
 
-const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
 
 export const lucia = new Lucia(adapter, {
 	sessionCookie: {
@@ -13,10 +13,25 @@ export const lucia = new Lucia(adapter, {
 			secure: process.env.NODE_ENV === "production",
 		},
 	},
+	getUserAttributes: (attributes) => {
+		return {
+			name: attributes.name,
+			email: attributes.email,
+			hashedPassword: attributes.hashedPassword,
+		};
+	},
+
 });
 
 declare module "lucia" {
 	interface Register {
 		Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
 	}
+}
+
+interface DatabaseUserAttributes {
+	name: string;
+	email: string;
+	hashedPassword: string;
 }
